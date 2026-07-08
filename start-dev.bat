be36736fa0693e25fa3caeb01cf6c49889fb3eaa
WN W <tiffany@WNdeMacBook-Air.local>
2026-07-08 16:02:11 +0800

    Clean up leftover scratch files and unused Windows batch script

--- start-dev.bat
+++
@@ -1,5 +0,0 @@
-@echo off
-set PATH=%PATH%;C:\Program Files\nodejs
-set NODE_ENV=development
-cd /d "D:\Car Rental - App"
-"C:\Program Files\nodejs\node.exe" "D:\Car Rental - App\node_modules\next\dist\bin\next" dev
